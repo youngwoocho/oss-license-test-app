1cf029621e5d0ef4a32eb6f46b439971e6a819e8
조영우 <youngchorg@gmail.com>
2022-03-20 18:52:21 +0900

    Add initial functional example

--- app/src/main/java/com/example/osslicensetestapp/MainActivity.kt
+++ app/src/main/java/com/example/osslicensetestapp/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.osslicensetestapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.osslicensetestapp.databinding.ActivityMainBinding
+import com.mikepenz.aboutlibraries.LibsBuilder
+import kotlinx.coroutines.NonCancellable.start
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+        
+        setUpShowOssButton()
+    }
+
+    private fun setUpShowOssButton() {
+        viewBinding.showOssButton.setOnClickListener {
+            LibsBuilder().start(this)
+
+        }
+
     }
 }
